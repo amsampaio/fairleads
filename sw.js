bcbe9d1bb37688b2f7c0e2ceee7bdf0a0cc9db4b
amsampaio <107897222+amsampaio@users.noreply.github.com>
2026-05-29 13:55:46 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'fairleads-v14';
+const CACHE = 'fairleads-v15';
 const CORE  = ['./index.html', './manifest.json', './icon.svg', './icon.ico', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
