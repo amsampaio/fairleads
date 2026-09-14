6012dec490662372b915fd59598987213dcdbc89
Alexandre Sampaio <alexandre.sampaio@eac.local>
2026-09-14 10:06:16 +0100

    Fix QR: confirmação dupla antes de disparar

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'fairleads-v20';
+const CACHE = 'fairleads-v21';
 const CORE  = ['./index.html', './manifest.json', './icon.svg', './icon.ico', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
